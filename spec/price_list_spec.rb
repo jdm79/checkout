00e8a9dfd3a85e37fd4451c8f0c848153b56d496
jdm79 <james.malvern@gmail.com>
2018-08-23 10:41:32 +0100

    adds price_list.rb require at the top of test, test passes

--- spec/price_list_spec.rb
+++ spec/price_list_spec.rb
@@ -1,5 +1,7 @@
+require './data/price_list.rb'
+
 describe 'price list' do
   it "shows a price list" do
-    expect(price_list).to be_a_kind_of(Hash)
+    expect(PRICE_LIST).to be_a_kind_of(Hash)
   end
 end
