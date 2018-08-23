6822afad4b329d7d2862bf4345eadf2be272c4db
jdm79 <james.malvern@gmail.com>
2018-08-23 10:39:26 +0100

    adds a price list hash in data repo

--- data/price_list.rb
+++ data/price_list.rb
@@ -1,3 +1,13 @@
 PRICE_LIST = {
-  
+  "apple": 0.45,
+  "pear": 0.50,
+  "orange": 0.50,
+  "mango": 0.70,
+  "coconut": 1.95,
+  "pineapple": 1.95,
+  "banana": 0.60,
+  "plum": 0.60,
+  "bag of grapes": 1.00,
+  "can of beer": 1.95,
+  "a cheese": 1.40 
 }
